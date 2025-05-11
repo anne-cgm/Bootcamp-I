773d2ff10b445594c9583ef64deb9f3839f597d3
annans95 <anna.95jay@gmail.com>
2025-05-11 12:56:31 -0300

    versão atualizada por fork

--- Python/Projeto VI/altura e genero.py
+++ Python/Projeto VI/altura e genero.py
@@ -1,25 +1,36 @@
-#Código que tem problema
 print('Digite [0] para sair')
-menor=999999999
-maior=-999999999
-altura=0
-f=0
-m=0
+ct=0
+menor = 3
+maior = 0
+f = 0
+m = 0
+
 while True:
-    genero=str(input('Digite seu gênero (feminino ou masculino):'))
-    altura=float(input('Digite sua altura:'))
+    genero = input('Digite seu gênero (feminino ou masculino): ').lower() # utilizado para transformar todas as letras de uma string em minúsculas
+    if genero == '0':
+        break
+    if genero not in ['feminino', 'masculino']:
+        print("Gênero inválido! Digite 'feminino' ou 'masculino'.")
+        continue
+
+    altura = float(input('Digite sua altura: '))
     if altura == 0:
         break
+
     if altura < menor:
-        menor=altura
+        menor = altura
     if altura > maior:
-        maior=altura
+        maior = altura
+
     if genero == 'feminino':
-        f=f+1
-    if genero == 'masculino':
-        m=m+1
-print('Maior altura do grupo:',maior)
-print('Menor altura do grupo:', menor)
-print('Quantidade de mulheres do grupo:', f)
-print('Quantidade de homens do grupo:', m)
+        f += 1
+    elif genero == 'masculino':
+        m += 1
+    ct = ct + 1
+
+if ct>0:
+    print('Maior altura do grupo:', maior)
+    print('Menor altura do grupo:', menor)
+    print('Quantidade de mulheres do grupo:', f)
+    print('Quantidade de homens do grupo:', m)
 
